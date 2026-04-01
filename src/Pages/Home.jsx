c8d262c64d318577e552704504e3595548f76297
dermawan22 <dermawan@alto.id>
2026-04-01 16:09:12 +0700

    v.3.0.11

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -24,14 +24,14 @@ const MainTitle = memo(() => (
       <span className="relative inline-block">
         <span className="absolute -inset-2 bg-gradient-to-r from-[#6366f1] to-[#a855f7] blur-2xl opacity-20"></span>
         <span className="relative bg-gradient-to-r from-white via-blue-100 to-purple-200 bg-clip-text text-transparent">
-          Frontend
+         Dermawan
         </span>
       </span>
       <br />
       <span className="relative inline-block mt-2">
         <span className="absolute -inset-2 bg-gradient-to-r from-[#6366f1] to-[#a855f7] blur-2xl opacity-20"></span>
         <span className="relative bg-gradient-to-r from-[#6366f1] to-[#a855f7] bg-clip-text text-transparent">
-          Developer
+          Suprihatin
         </span>
       </span>
     </h1>
@@ -76,8 +76,8 @@ const SocialLink = memo(({ icon: Icon, link, label }) => (
 const TYPING_SPEED = 100;
 const ERASING_SPEED = 50;
 const PAUSE_DURATION = 2000;
-const WORDS = ["Network & Telecom Student", "Tech Enthusiast"];
-const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind"];
+const WORDS = ["Passionate IT Professional", "Tech Enthusiast"];
+const TECH_STACK = ["API" , "ISO 8583", "QA", "Financial Technology"];
 const SOCIAL_LINKS = [
   { icon: Github, link: "https://github.com/dermawans", label: "GitHub Profile" },
   { icon: Linkedin, link: "https://www.linkedin.com/in/dermawansuprihatin/", label: "LinkedIn Profile" },
